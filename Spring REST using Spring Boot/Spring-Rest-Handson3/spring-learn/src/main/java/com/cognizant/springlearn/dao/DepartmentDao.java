0d801d1c83362b22460c3e8212af1e72b1be9cfa
Nohitha567 <nohithachede@gmail.com>
2026-07-06 23:58:58 +0530

    Completed Hands-on 2

--- Spring REST using Spring Boot/Spring-Rest-Handson3/spring-learn/src/main/java/com/cognizant/springlearn/dao/DepartmentDao.java
+++ Spring REST using Spring Boot/Spring-Rest-Handson3/spring-learn/src/main/java/com/cognizant/springlearn/dao/DepartmentDao.java
@@ -1,4 +1,29 @@
 package com.cognizant.springlearn.dao;
 
+import com.cognizant.springlearn.model.Department;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
 public class DepartmentDao {
+
+  private static List<Department> DEPARTMENT_LIST = new ArrayList<>();
+
+  @SuppressWarnings("unchecked")
+  public DepartmentDao() {
+
+    ApplicationContext context =
+      new ClassPathXmlApplicationContext("employee.xml");
+
+    DEPARTMENT_LIST =
+      (ArrayList<Department>) context.getBean("departmentList");
+  }
+
+  public List<Department> getAllDepartments() {
+    return DEPARTMENT_LIST;
+  }
 }
