eb6bdaf637410638f56bc4d78ba2e94eb5c83c76
Nohitha567 <nohithachede@gmail.com>
2026-07-06 20:31:52 +0530

    Completed Spring REST Hands-on 2

--- SpringDataJPA_with_Hibernate/Hands_On_4/spring-learn/src/main/java/com/cognizant/springlearn/SpringLearnApplication.java
+++ SpringDataJPA_with_Hibernate/Hands_On_4/spring-learn/src/main/java/com/cognizant/springlearn/SpringLearnApplication.java
@@ -4,6 +4,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 @SpringBootApplication
 public class SpringLearnApplication {
@@ -13,11 +18,32 @@ public class SpringLearnApplication {
 
   public static void main(String[] args) {
 
-    LOGGER.info("START");
-
     SpringApplication.run(SpringLearnApplication.class, args);
 
-    LOGGER.info("Spring Boot Application Started Successfully");
+    displayDate();
+  }
+
+  public static void displayDate() {
+
+    LOGGER.info("START");
+
+    try {
+
+      ApplicationContext context =
+        new ClassPathXmlApplicationContext("date-format.xml");
+
+      SimpleDateFormat format =
+        context.getBean("dateFormat", SimpleDateFormat.class);
+
+      Date date = format.parse("31/12/2018");
+
+      LOGGER.info("Parsed Date : {}", date);
+
+    } catch (Exception e) {
+
+      LOGGER.error("Error : ", e);
+
+    }
 
     LOGGER.info("END");
   }
