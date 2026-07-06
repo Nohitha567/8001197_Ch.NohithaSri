9e730886febaef86696762d8ed70f35127173666
Nohitha567 <nohithachede@gmail.com>
2026-07-06 20:45:29 +0530

    Completed Spring REST Hands-on 6

--- SpringDataJPA_with_Hibernate/Hands_On_4/spring-learn/src/main/java/com/cognizant/springlearn/SpringLearnApplication.java
+++ SpringDataJPA_with_Hibernate/Hands_On_4/spring-learn/src/main/java/com/cognizant/springlearn/SpringLearnApplication.java
@@ -8,6 +8,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+
 @SpringBootApplication
 public class SpringLearnApplication {
 
@@ -18,25 +22,78 @@ public class SpringLearnApplication {
 
     SpringApplication.run(SpringLearnApplication.class, args);
 
+    displayDate();
+
     displayCountry();
+
+    displayCountries();
   }
 
+  // Hands-on 2
+  public static void displayDate() {
+
+    LOGGER.info("START - displayDate");
+
+    try {
+
+      ApplicationContext context =
+        new ClassPathXmlApplicationContext("date-format.xml");
+
+      SimpleDateFormat format =
+        context.getBean("dateFormat", SimpleDateFormat.class);
+
+      Date date = format.parse("31/12/2018");
+
+      LOGGER.debug("Parsed Date : {}", date);
+
+    } catch (Exception e) {
+
+      LOGGER.error("Exception occurred", e);
+
+    }
+
+    LOGGER.info("END - displayDate");
+  }
+
+  // Hands-on 4 & 5
   public static void displayCountry() {
 
-    LOGGER.info("START");
+    LOGGER.info("START - displayCountry");
 
     ApplicationContext context =
       new ClassPathXmlApplicationContext("country.xml");
 
-    Country country = context.getBean("country", Country.class);
+    Country country = context.getBean("in", Country.class);
 
-    Country anotherCountry = context.getBean("country", Country.class);
+    Country anotherCountry = context.getBean("in", Country.class);
 
-    LOGGER.info("Country 1 : {}", country);
-    LOGGER.info("Country 2 : {}", anotherCountry);
+    LOGGER.debug("Country 1 : {}", country);
 
-    LOGGER.info("Same Object : {}", country == anotherCountry);
+    LOGGER.debug("Country 2 : {}", anotherCountry);
 
-    LOGGER.info("END");
+    LOGGER.debug("Same Object : {}", country == anotherCountry);
+
+    LOGGER.info("END - displayCountry");
+  }
+
+  // Hands-on 6
+  @SuppressWarnings("unchecked")
+  public static void displayCountries() {
+
+    LOGGER.info("START - displayCountries");
+
+    ApplicationContext context =
+      new ClassPathXmlApplicationContext("country.xml");
+
+    List<Country> countries =
+      (List<Country>) context.getBean("countryList");
+
+    for (Country country : countries) {
+
+      LOGGER.debug("{}", country);
+
+    }
+
+    LOGGER.info("END - displayCountries");
   }
 }
