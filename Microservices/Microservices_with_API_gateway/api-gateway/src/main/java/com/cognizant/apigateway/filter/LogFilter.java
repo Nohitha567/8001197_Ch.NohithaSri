e084352426a2fc642d6f5d6d1ce60dd8c1c49f40
Nohitha567 <nohithachede@gmail.com>
2026-07-13 18:16:06 +0530

    Completed API Gateway Global Logging Filter

---
+++ Microservices/Microservices_with_API_gateway/api-gateway/src/main/java/com/cognizant/apigateway/filter/LogFilter.java
@@ -0,0 +1,40 @@
+package com.cognizant.apigateway.filter;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+@Component
+public class LogFilter implements GlobalFilter, Ordered {
+
+  private static final Logger LOGGER =
+    LoggerFactory.getLogger(LogFilter.class);
+
+  @Override
+  public Mono<Void> filter(ServerWebExchange exchange,
+                           GatewayFilterChain chain) {
+
+    LOGGER.info("Incoming Request");
+
+    LOGGER.info("Method : {}",
+      exchange.getRequest().getMethod());
+
+    LOGGER.info("URI : {}",
+      exchange.getRequest().getURI());
+
+    LOGGER.info("Path : {}",
+      exchange.getRequest().getPath());
+
+    return chain.filter(exchange);
+  }
+
+  @Override
+  public int getOrder() {
+    return 1;
+  }
+}
